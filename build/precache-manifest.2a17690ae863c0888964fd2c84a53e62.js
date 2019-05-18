7ee31b9c7bd2758d737779dc0c07e51a749b8d3c
nvallepalli1 <nvallepalli@gmail.com>
2019-05-17 19:42:40 -0500

    initial commit with dockerfiles

---
+++ build/precache-manifest.2a17690ae863c0888964fd2c84a53e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6eb463583c0a4bc2e01c",
+    "url": "/static/js/main.f1a2fb1b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6eb463583c0a4bc2e01c",
+    "url": "/static/css/main.616accdd.chunk.css"
+  },
+  {
+    "revision": "994878e6a88fceb596e278fead7a7775",
+    "url": "/index.html"
+  }
+];
